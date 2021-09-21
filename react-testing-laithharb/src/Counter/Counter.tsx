7eb7314082dbfb691fb41fc6d1eca297a95f6a53
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2021-09-21 14:48:20 +0700

    [react-testing-laithharb] fire an event

--- react-testing-laithharb/src/Counter/Counter.tsx
+++ react-testing-laithharb/src/Counter/Counter.tsx
@@ -1,23 +1,48 @@
 import React, { useState } from "react";
+import "./Counter.css";
 
 const Counter = () => {
   const [counterValue, setCounterValue] = useState<number>(0);
   const [inputValue, setInputValue] = useState<number>(1);
 
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(parseInt(e.target.value));
+  };
+
+  const addBtnHandler = () => {
+    setCounterValue((prevValue) => prevValue + inputValue);
+  };
+
+  const subtractBtnHandler = () => {
+    setCounterValue((prevValue) => prevValue - inputValue);
+  };
+
   return (
     <div>
       <h1 data-testid="header">My Counter</h1>
 
-      <h2 data-testid="counter">{counterValue}</h2>
+      <h2
+        data-testid="counter"
+        className={`${counterValue >= 100 ? "green" : ""}${
+          counterValue <= -100 ? "red" : ""
+        }`}
+      >
+        {counterValue}
+      </h2>
 
-      <button data-testid="subtract-btn">-</button>
+      <button data-testid="subtract-btn" onClick={subtractBtnHandler}>
+        -
+      </button>
       <input
         type="number"
         data-testid="input"
+        className="text-center"
         value={inputValue}
-        style={{ textAlign: "center" }}
+        onChange={changeHandler}
       />
-      <button data-testid="add-btn">+</button>
+      <button data-testid="add-btn" onClick={addBtnHandler}>
+        +
+      </button>
     </div>
   );
 };
--- react-testing-laithharb/src/Counter/__test__/Counter.test.tsx
+++ react-testing-laithharb/src/Counter/__test__/Counter.test.tsx
@@ -1,5 +1,5 @@
 import Counter from "../Counter";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 test("header renders with correct text", () => {
@@ -40,4 +40,136 @@ test("subtract button renders with -", () => {
 
   // assertion
   expect(subtractBtn.textContent).toBe("-");
+});
+
+// Testing functionality
+test("change value of input works correctly", () => {
+  const { getByTestId } = render(<Counter />);
+  const inputElm = getByTestId("input");
+
+  // assertion at intial render
+  expect(inputElm).toHaveValue(1);
+
+  // fire an event
+  fireEvent.change(inputElm, { target: { value: 5 } });
+
+  // assertion after fire an event
+  expect(inputElm).toHaveValue(5);
+});
+
+test("click on add button adds 1 to counter", () => {
+  const { getByTestId } = render(<Counter />);
+  const addBtn = getByTestId("add-btn");
+  const counterElm = getByTestId("counter");
+
+  // assertion at intial render
+  expect(counterElm.textContent).toBe("0");
+
+  // fire an event
+  fireEvent.click(addBtn);
+
+  // assertion after fire an event
+  expect(counterElm.textContent).toBe("1");
+});
+
+test("click on subtract button subtracts 1 to counter", () => {
+  const { getByTestId } = render(<Counter />);
+  const subtractBtn = getByTestId("subtract-btn");
+  const counterElm = getByTestId("counter");
+
+  // assertion at intial render
+  expect(counterElm.textContent).toBe("0");
+
+  // fire an event
+  fireEvent.click(subtractBtn);
+
+  // assertion after fire an event
+  expect(counterElm.textContent).toBe("-1");
+});
+
+test("changing input value then clicking on add button works correctly", () => {
+  const { getByTestId } = render(<Counter />);
+  const addBtn = getByTestId("add-btn");
+  const counterElm = getByTestId("counter");
+  const inputElm = getByTestId("input");
+
+  // fire events
+  fireEvent.change(inputElm, { target: { value: 5 } });
+  fireEvent.click(addBtn);
+
+  // assertion after fire an event
+  expect(counterElm.textContent).toBe("5");
+});
+
+test("changing input value then clicking on subtract button works correctly", () => {
+  const { getByTestId } = render(<Counter />);
+  const subtractBtn = getByTestId("subtract-btn");
+  const counterElm = getByTestId("counter");
+  const inputElm = getByTestId("input");
+
+  // fire events
+  fireEvent.change(inputElm, { target: { value: 5 } });
+  fireEvent.click(subtractBtn);
+
+  // assertion after fire an event
+  expect(counterElm.textContent).toBe("-5");
+});
+
+test("adding and then subtracting leads to the correct counter number", () => {
+  const { getByTestId } = render(<Counter />);
+  const addBtn = getByTestId("add-btn");
+  const subtractBtn = getByTestId("subtract-btn");
+  const counterElm = getByTestId("counter");
+  const inputElm = getByTestId("input");
+
+  // fire events
+  fireEvent.change(inputElm, { target: { value: 10 } });
+  fireEvent.click(addBtn);
+  fireEvent.click(addBtn);
+  fireEvent.click(addBtn);
+  fireEvent.click(addBtn);
+  fireEvent.click(subtractBtn);
+  fireEvent.click(subtractBtn);
+
+  // assertion
+  expect(counterElm.textContent).toBe("20");
+
+  // fire events
+  fireEvent.change(inputElm, { target: { value: 5 } });
+  fireEvent.click(addBtn);
+  fireEvent.click(subtractBtn);
+  fireEvent.click(subtractBtn);
+
+  // assertion
+  expect(counterElm.textContent).toBe("15");
+});
+
+test("counter contains correct className", () => {
+  const { getByTestId } = render(<Counter />);
+  const addBtn = getByTestId("add-btn");
+  const subtractBtn = getByTestId("subtract-btn");
+  const counterElm = getByTestId("counter");
+  const inputElm = getByTestId("input");
+
+  expect(counterElm.className).toBe("");
+
+  fireEvent.change(inputElm, { target: { value: 50 } });
+  fireEvent.click(addBtn);
+  expect(counterElm.className).toBe("");
+
+  fireEvent.click(addBtn);
+  expect(counterElm.className).toBe("green");
+
+  fireEvent.click(addBtn);
+  expect(counterElm.className).toBe("green");
+
+  fireEvent.click(subtractBtn);
+  fireEvent.click(subtractBtn);
+  expect(counterElm.className).toBe("");
+
+  fireEvent.click(subtractBtn);
+  fireEvent.click(subtractBtn);
+  fireEvent.click(subtractBtn);
+  fireEvent.click(subtractBtn);
+  expect(counterElm.className).toBe("red");
 });
