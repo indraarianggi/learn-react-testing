1dc59753fe0ccb6caf8592b17ee3ab0a97d911c3
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2021-09-22 21:09:45 +0700

    [react-testing-pmez] setting up and TDD on App component

--- react-testing-pmez/src/App.test.tsx
+++ react-testing-pmez/src/App.test.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import PersonLists from "./components/PersonLists";
+
+describe("App", () => {
+  let appWrapper: any;
+
+  beforeAll(() => {
+    appWrapper = shallow(<App />);
+  });
+
+  it("renders a person list", () => {
+    const personList = appWrapper.find(PersonLists);
+
+    expect(personList).toHaveLength(1);
+  });
+
+  it("has state", () => {
+    const appState = appWrapper.state();
+
+    expect(appState).not.toBeNull();
+  });
+
+  it("has a people property on state", () => {
+    const appState: any = appWrapper.state();
+
+    expect(appState.people).toBeDefined();
+  });
+
+  it("passes people property of state to personList as prop", () => {
+    const personList = appWrapper.find(PersonLists);
+
+    expect(personList.props().people).toEqual(appWrapper.state().people);
+  });
 });
